26aea075bc691655d3423a766d1cd0a22cabd0d6
Mark Windsor <mark.windsor@fytics.com>
2019-07-19 10:50:15 -0230

    Add Contains Duplicate problem

---
+++ Python/PythonAlgorithms.py
@@ -0,0 +1,19 @@
+# Python - Algorithms
+
+"""
+An algorithm is a step by step process which defines a set of instructions to be executed to get the desired output
+
+Some important categories of algorithms are:
+
+1. Search : An algorithm to search for an item in a data structure
+2. Sort : An algorithm to sort items in a certain order
+3. Insert : Algorithm to insert an item in a data structure
+4. Update : Algorithm to update an existing item in a data structure
+5. Delete : Algorithm to delete an existing item from a data structure
+
+"""
+
+#__________________________________________________________________________________________________________________
+# Merge Sort
+# Merge sort first divides the array into equal halves and then combines them in a sorted manner
+
