dd50e89b7c1df903a9e9a1a8ff72a59bdc7c821e
Mark Windsor <mark.windsor@vision33.com>
2020-01-29 14:15:19 -0330

    d

---
+++ Algorithms/Python Algorithms/BinarySearch.py
@@ -0,0 +1,12 @@
+# Iterative Approach of Binary Search
+
+def binary_search_iterative(a_list, left, right, x):
+
+	while left <= right:
+
+		middle = 
+
+
+# Iterative Approach of Binary Search
+
+def binary_search_recursive(a_list, left, right, x):
